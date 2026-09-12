b771389a7ad7b73d61e8dfa6fc333768bee9ef3a
Nihal-kappungal <nihalkappungal.workspace@gmail.com>
2026-09-12 15:07:15 +0530

    layout responsive

--- lessons/09-advance-router/src/pages/Home.jsx
+++ lessons/09-advance-router/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import MacFolder, { colorMap } from "../components/MacFolder";
 
 const Home = () => {
   return (
-    <div className="grid grid-cols-4 gap-10 items-center justify-center p-10 min-h-screen bg-[#f5f5f7]">
+    <div className="grid xl:grid-cols-4 gap-10 items-center sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center p-10 min-h-screen bg-[#f5f5f7] dark:bg-black">
       {Object.keys(colorMap).map((color) => (
         <div className="flex items-center justify-center flex-col gap-6">
           <MacFolder key={color} label="Documents" count={24} color={color} />
