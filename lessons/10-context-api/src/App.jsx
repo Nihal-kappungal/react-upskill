ad2cdfc92c40c3f00641fc974449e86587bac5a0
Nihal-kappungal <nihalkappungal.workspace@gmail.com>
2026-09-14 20:37:53 +0530

    fix context api folder tracking

---
+++ lessons/10-context-api/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div
+      className="w-full min-h-screen bg-sky-400 flex items-center justify-center
+  "
+    >
+      App
+    </div>
+  );
+};
+
+export default App;
