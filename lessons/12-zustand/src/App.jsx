b67459e53b1834a93c4b4a28f64ef0a3eda29be5
Nihal-kappungal <nihalkappungal.workspace@gmail.com>
2026-09-16 11:40:18 +0530

    implemented simple zustand store

--- lessons/12-zustand/src/App.jsx
+++ lessons/12-zustand/src/App.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import { useCounterStore } from "./store/CounterStore";
 
 const App = () => {
+  const count = useCounterStore((state) => state.count);
+
+  const increment = useCounterStore((state) => state.increment);
+
   return (
-    <div className="bg-sky-400 w-full min-h-screen flex items-center justify-center text-6xl font-bold">
-      App
+    <div className="w-full min-h-screen bg-green-400 flex items-center justify-center flex-col gap-8">
+      <h1>Count : {count}</h1>
+      <button
+        onClick={increment}
+        className="px-4 py-3 bg-sky-400 text-sm rounded-full font-bold text-white"
+      >
+        Add
+      </button>
     </div>
   );
 };
