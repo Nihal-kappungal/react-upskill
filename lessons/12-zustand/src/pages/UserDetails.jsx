0ebf32221d57215edb6b57069c5beffdf6bc0164
Nihal-kappungal <nihalkappungal.workspace@gmail.com>
2026-09-16 14:15:15 +0530

    simple api fetch with zustand

--- lessons/12-zustand/src/pages/UserDetails.jsx
+++ lessons/12-zustand/src/pages/UserDetails.jsx
@@ -1,32 +1,24 @@
-import React, { useState } from "react";
+import { useEffect } from "react";
 import { useUserDetailsStore } from "../store/UserDetailsStore";
 
 const UserDetails = () => {
-  const name = useUserDetailsStore((state) => state.name);
-  const age = useUserDetailsStore((state) => state.age);
-  const setName = useUserDetailsStore((state) => state.setName);
-  const setAge = useUserDetailsStore((state) => state.setAge);
-  const [active, setActive] = useState(false);
+  const { user, loading, error, fetchDetails } = useUserDetailsStore();
 
-  const handleUserDetails = () => {
-    setName();
-    setAge();
-    setActive((prev) => !prev);
-  };
+  useEffect(() => {
+    fetchDetails(1);
+    console.log(user);
+  }, []);
+
+  if (loading) return <p>loading .....</p>;
+  if (error) return <p>{error}</p>;
+
   return (
     <section className="w-full min-h-screen bg-zinc-800 flex items-center justify-center gap-6 text-white flex-col">
-      <button
-        onClick={handleUserDetails}
-        className={`px-4 py-3 ${active === false ? "bg-green-400" : "bg-red-400"} bg-green-400 rounded text-sm font-black`}
-      >
-        {active == false ? "view user " : "hide user"}
-      </button>
-      {active && (
-        <div className="p-10 text-2xl bg-violet-400 flex items-center justify-center flex-col rounded-4xl ">
-          <h1>name : {name} </h1>
-          <h3>age : {age} </h3>
-        </div>
-      )}
+      <div className="p-10 text-2xl bg-violet-400 flex items-center justify-center flex-col rounded-4xl ">
+        <h1>name :{user.name} </h1>
+        <h3>email :{user.email} </h3>
+        <h3>phone :{user.phone} </h3>
+      </div>
     </section>
   );
 };
--- lessons/12-zustand/src/store/UserDetailsStore.js
+++ lessons/12-zustand/src/store/UserDetailsStore.js
@@ -1,9 +1,21 @@
 import { create } from "zustand";
 
 export const useUserDetailsStore = create((set) => ({
-  name: null,
-  age: null,
+  user: "",
+  loading: "",
+  error: null,
 
-  setName: () => set({ name: "smith" }),
-  setAge: () => set({ age: 40 }),
+  fetchDetails: async (id) => {
+    set({ loading: true });
+    try {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/users/${id}`,
+      );
+      const data = await response.json();
+
+      set({ user: data, loading: false });
+    } catch (error) {
+      set({ error: "Failed to Fetch Data", loading: false });
+    }
+  },
 }));
