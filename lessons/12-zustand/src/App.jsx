7c81e80ef9f455fa559a01e47006483b10d22673
Nihal-kappungal <nihalkappungal.workspace@gmail.com>
2026-09-16 10:26:58 +0530

    12-zustand first commit

---
+++ lessons/12-zustand/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-sky-400 w-full min-h-screen flex items-center justify-center text-6xl font-bold">
+      App
+    </div>
+  );
+};
+
+export default App;
