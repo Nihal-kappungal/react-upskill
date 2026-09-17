d6807d8d9f546346b1d38521042bc46abe0f4a3d
Nihal-kappungal <nihalkappungal.workspace@gmail.com>
2026-09-17 19:40:34 +0530

    basic form handling

---
+++ lessons/13-advance-forms/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import From from "./components/From";
+
+const App = () => {
+  return (
+    <div className="w-full min-h-screen bg-zinc-800">
+      <From />
+    </div>
+  );
+};
+
+export default App;
