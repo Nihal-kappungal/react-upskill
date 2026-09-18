e2fc289286895bab94fae6a1f4e942db6f339102
Nihal-kappungal <nihalkappungal.workspace@gmail.com>
2026-09-18 21:27:43 +0530

    fix: added more condition for password

--- lessons/14-formHook-zod/src/schemas/registerSchema.js
+++ lessons/14-formHook-zod/src/schemas/registerSchema.js
@@ -4,7 +4,11 @@ export const registerSchema = z
   .object({
     name: z.string().min(4, "Name need atleast one character"),
     email: z.email("enter valid email"),
-    password: z.string().min(8, "password must be 8 character"),
+    password: z
+      .string()
+      .min(8, "password must be 8 character")
+      .regex(/[A-Z]/, "Password must contain an uppercase")
+      .regex(/[^A-Za-z0-9]/, "Password must contain a special character"),
     confirmPassword: z.string(),
   })
   .refine((data) => data.password === data.confirmPassword, {
