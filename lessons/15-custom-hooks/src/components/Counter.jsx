9058f03bbd67a94baf568bb21ac5d941c3d209eb
Nihal-kappungal <nihalkappungal.workspace@gmail.com>
2026-09-19 09:37:50 +0530

    refactor: separate the counter component

---
+++ lessons/15-custom-hooks/src/components/Counter.jsx
@@ -0,0 +1,28 @@
+import useCounter from "../hooks/useCounter";
+
+const Counter = () => {
+  const { count, increment, decrement } = useCounter();
+  return (
+    <section className="w-full min-h-screen bg-green-400 flex items-center justify-center flex-col gap-10">
+      <h3 className="text-6xl p-10 bg-green-900 rounded-2xl text-white">
+        {count}
+      </h3>
+      <div className="flex items-center justify-center gap-4">
+        <button
+          onClick={increment}
+          className="bg-green-900 text-white px-4 py-3 text-sm font-bold rounded-full"
+        >
+          Increment
+        </button>
+        <button
+          onClick={decrement}
+          className="bg-green-900 text-white px-4 py-3 text-sm font-bold rounded-full"
+        >
+          decrement
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default Counter;
